3fb647e702939bf350d7562d763f02da587533c0
Euzkaro <gallasteguicrino@MacBook-Pro-de-Gallastegui.local>
2019-03-29 17:55:17 -0500

    map updated to contain demographic data in popups

--- static/js/main-map.js
+++ static/js/main-map.js
@@ -10,14 +10,14 @@ var map = L.map("main-map").setView([39.381266, -97.922211], 4);
     accessToken: "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ"
   }).addTo(map);
 
+
+
+//##################################################################################
 //   //GeoData State Level
-var statesData = "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json"
+//##################################################################################
+//var statesData = "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json"
+  var statesData = "https://raw.githubusercontent.com/Euzkaro/project2.io/master/state-demgraphics.json"
 
-//   L.geoJson(statesData).addTo(map);
-
-//   L.marker([39.381266, -97.922211]).addTo(map)
-//     .bindPopup('<h1>USA.</h1><hr><h3> #1 Trending Topic: Jeff, help me!.</h3>')
-//     .openPopup();
 
 // Grabbing our GeoJSON data..
 d3.json(statesData, function(data) {
@@ -49,11 +49,26 @@ d3.json(statesData, function(data) {
         }
       });
       // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<h1>" + feature.properties.name + "</h1> <hr> <h3> #1 Trending Topic: Basic Map Done!.</h3>");
+      layer.bindPopup("<h1>" + feature.properties.name + 
+      "</h1> <hr> <p>Population: " + 
+      feature.properties.Population + 
+      "<br> Republican Voters (%): " + feature.properties.Republican + 
+      "<br> Democrat Voters (%): " + feature.properties.Democrat +
+      "<br> College Graduates (%): " + feature.properties.BachelorDegree +
+      "<br> High School Graduates (%): " + feature.properties.HighSchool +
+      "<br> Whites (%): " + feature.properties.White +
+      "<br> Blacks (%): " + feature.properties.Black +
+      "<br> Asians (%): " + feature.properties.Asian +
+      "<br> Latinos (%): " + feature.properties.Latino +
+      "<br> Natives (%): " + feature.properties.Native +
+      "<br> Unemployment (%): " + feature.properties.Unemployment +
+      "</p>");
 
     }
   }).addTo(map);
 });
+//##################################################################################
+
 
 
   
