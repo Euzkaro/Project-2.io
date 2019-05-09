0ec111681871446bf8f5a4b5b2b148e214335eab
Jeffery Brown <39058795+daddyjab@users.noreply.github.com>
2019-05-08 21:27:21 -0500

    helper scripts to run updates offline - 2nd try for tweets

--- update_tweets.py
+++ update_tweets.py
@@ -1,6 +1,12 @@
 # Initial the database on Heroku start-up
-from python.app import update_db_tweets_table
+from python.app import get_tweet_list, update_db_tweets_table
 
 print("PERFORMING UPDATE: Tweets Table")
-n_tweets = update_db_tweets_table()
-print(n_tweets)
+
+# Get the list of Tweets in the Trends table
+tweet_list = get_tweet_list()
+
+# Update the tweets table for this list of tweets (add new tweets, and update existig tweets)
+update_status = update_db_tweets_table(tweet_list)
+
+print(update_status)
